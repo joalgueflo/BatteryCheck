0098f26760959582252b13a7ebbe42504d2dcbcc
joalgueflo <114319054+joalgueflo@users.noreply.github.com>
2023-08-10 15:09:53 -0600

    first commit

---
+++ batterySound.py
@@ -0,0 +1,35 @@
+import psutil
+import time
+import pygame
+
+#Testing
+
+#Function that searches for the file with the sound and plays it
+def play_sound(sound_file):
+    pygame.init()
+    sound = pygame.mixer.Sound(sound_file)
+    sound.play()
+    pygame.time.wait(3000)  # Play the sound for 3 seconds
+    sound.stop()
+    pygame.quit()
+
+#Pathname to sound file
+#In sound_file you can add your own path to the sound that you want
+#Im still learning :)
+sound_file = '/Users/joseguerrero/Downloads/new-notification-138807.mp3'
+percent = 10
+lBatteryCheck = int (0)
+while True:
+    #For every loop, checks the battery bercentage and stores it...
+    battery = psutil.sensors_battery() 
+    percent_left = int(battery.percent) # <---- Here
+    #If baterry perfecntage is at a number divisible by 10, it plays the sound to notify the user that 
+    #battery has decreased a 10 percent
+
+    if (percent_left % 10 == 0 and lBatteryCheck != percent_left): 
+        
+        play_sound(sound_file)
+        lBatteryCheck = percent_left
+        
+    else:
+        time.sleep(241)
